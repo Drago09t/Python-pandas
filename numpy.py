52b1d33a35d6a53bc98c969cb6af41126b32d46b
Drago09t <92865043+Drago09t@users.noreply.github.com>
2024-05-19 16:36:38 +0500

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,8 @@
+import numpy as np
+ heights = np.array([65, 68, 70, 62, 72, 69, 74, 61, 63, 66])
+ mean_height = np.mean(heights)
+ median_height = np.median(heights)
+ std_dev_height = np.std(heights)
+ print("Mean height:", mean_height)
+ print("Median height:", median_height)
+ print("Standard deviation of height:", std_dev_height)
